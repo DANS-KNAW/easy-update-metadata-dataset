1c5a2a5234449cced36bc0f5bac7470922622337
Richard van Heest <rvanheest@users.noreply.github.com>
2016-11-07 17:44:18 +0100

    upgrade scallop to 2.0.5 (#3)
    * upgrade scallop to 2.0.2
    
    * update README
    
    * upgrade scallop to 2.0.5

--- src/main/scala/nl/knaw/dans/easy/umd/CommandLineOptions.scala
+++ src/main/scala/nl/knaw/dans/easy/umd/CommandLineOptions.scala
@@ -45,49 +45,28 @@ class CommandLineOptions(args: Array[String] = "-ss -tt -f http:// src/test/reso
        |Options:
        |""".stripMargin)
 
-  private val shouldBeFile = singleArgConverter(value =>
-    new File(value) match {
-      case f if f.isFile => f
-      case _ => throw createExecption(s"'$value' is not a file")
-    }
-  )
-  private val shouldBeDir = singleArgConverter(value =>
-    new File(value) match {
-      case f if f.isDirectory => f
-      case _ => throw createExecption(s"'$value' is not a directory")
-    }
-  )
-  private val shouldBeUrl = singleArgConverter(value =>
-    Try {
-      new URL(value)
-    } match {
-      case Success(url) => value
-      case Failure(e) => throw createExecption(s"'$value' is not a valid url: ${e.getMessage}")
-    }
-  )
-
-  private def createExecption(msg: String) = {
-    log.error(msg)
-    new IllegalArgumentException(msg)
-  }
-
-
   val doUpdate = opt[Boolean](name = "doUpdate", noshort = true,
     descr = "Without this argument no changes are made to the repository, the default is a test mode that logs the intended changes",
     default = Some(false))
 
-  val streamID = opt[String](name = "stream-id", short = 's', required = true, descr = "id of fedoara stream to update")
-  val tag = opt[String](name = "tag", short = 't', required = true, descr = "xml tag to change")
+  val streamID = opt[String](name = "stream-id", short = 's', required = true,
+    descr = "id of fedoara stream to update")
+  val tag = opt[String](name = "tag", short = 't', required = true,
+    descr = "xml tag to change")
 
-  val fedoraUrl = opt[String](name = "fedora-url", short = 'f',
+  val fedoraUrl = opt[URL](name = "fedora-url", short = 'f',
     descr = "Base url for the fedora repository",
-    default = Some("http://localhost:8080/fedora"))(shouldBeUrl)
+    default = Some(new URL("http://localhost:8080/fedora")))
   val fedoraUsername = opt[String](name = "fedora-username", noshort = true,
     descr = "Username for fedora repository, if omitted provide it on stdin")
   val fedoraPassword = opt[String](name = "fedora-password", noshort = true,
     descr = "Password for fedora repository, if omitted provide it on stdin")
 
-  val inputFile = trailArg[File](name = "input-file", required = true, descr = "The CSV file with required changes. Columns: fedoraID, newValue. First line is assumed to be a header.")(shouldBeFile)
+  val inputFile = trailArg[File](name = "input-file", required = true,
+    descr = "The CSV file with required changes. Columns: fedoraID, newValue. First line is assumed to be a header.")
+
+  validateFileExists(inputFile)
+  validateFileIsFile(inputFile)
 
   footer("")
   verify()
@@ -101,10 +80,9 @@ object CommandLineOptions {
     log.debug("Parsing command line ...")
     val opts = new CommandLineOptions(args)
 
-    val fedoraUrl = new URL(opts.fedoraUrl())
-
-    val fedoraUser = opts.fedoraUsername.get.getOrElse(ask(fedoraUrl.toString, "user name"))
-    val fedoraPassword = opts.fedoraPassword.get.getOrElse(askPassword(fedoraUser, fedoraUrl.toString))
+    val fedoraUrl = opts.fedoraUrl()
+    val fedoraUser = opts.fedoraUsername.toOption.getOrElse(ask(fedoraUrl.toString, "user name"))
+    val fedoraPassword = opts.fedoraPassword.toOption.getOrElse(askPassword(fedoraUser, fedoraUrl.toString))
     val fedoraCredentials = new FedoraCredentials(fedoraUrl, fedoraUser, fedoraPassword) {
       override def toString = s"fedoraURL=${fedoraUrl.toString}, user=$fedoraUser" // prevents logging a password
     }
